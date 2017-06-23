b5b4506d8da89390d87f9f46002de892c7ccf1c1
pgrossi <pgrossi@users.noreply.github.com>
2017-06-23 15:40:58 +0200

    adding changes to server.js

--- server.js
+++ server.js
@@ -12,36 +12,20 @@ const nodemailer = require("nodemailer");
 const cronJob = require("cron").CronJob;
 */
 
-// ---------------- Start Express setting for the bot --------------------------
+// ---------------- Start HTTP endpoint functions for the bot ------------------
 
-/*
-const express = require("express");
-const app = express();
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-*/
+function getChatPage(req, res, next) {
+  res.setHeader('Content-Type', 'text/html');
+  res.end("<html><title>Prova</title><iframe src='https://webchat.botframework.com/embed/deloitte_bot?s=OKQOYQdkMuQ.cwA.-UI.Zx34euxO10K1ST8hDEooUtR54Gyiw55SQr4Lt2Hhtoo'></iframe></html>");
+  next();
+}
 
-// ------------------ End Express setting for the bot --------------------------
+function getHealth(req, res, next) {
+  res.send('health OK.');
+  next();
+}
 
-// ---------------- Start Module setting for the bot ---------------------------
-
-/*
-const db = require(path.join(__dirname, "db.js"))();
-const dm = require(path.join(__dirname, "dm.js"))();
-const communicate = require(path.join(__dirname, "communicate.js"))();
-const support = require(path.join(__dirname, "support.js"))();
-*/
-
-// ------------------ End Module setting for the bot ---------------------------
-
-// ---------------- Start Mail setting for the bot -----------------------------
-
-/*
-const mail = require(path.join(__dirname, "mail", "mail.js"))();
-mail["daemonDailyMail"](path.join(__dirname, "database", db_final_value));
-*/
-
-// ------------------ End Mail setting for the bot -----------------------------
+// ------------------ End HTTP endpoint functions for the bot ------------------
 
 // ---------------- Start Microsoft setting for the bot ------------------------
 
@@ -49,8 +33,11 @@ var restify = require("restify");
 var builder = require("botbuilder");
 
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function() {
-   console.log("%s listening to %s", server.name, server.url);
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3978
+var ip = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0"
+
+server.listen(port, ip, function () {
+  console.log("%s listening to %s", server.name, server.url);
 });
 
 /*
@@ -65,21 +52,14 @@ var connector = new builder.ChatConnector({
     appPassword: "te2a62z5VUXp1d0H4ML4szz"
 });
 
-var bot = new builder.UniversalBot(connector, function(session) {
-    session.send("You said: %s", session.message.text);
-});
-
 server.post("/api/messages", connector.listen());
 
 // ------------------ End Microsoft setting for the bot ------------------------
 
-// ---------------- Start Server setting for the bot ---------------------------
+// ---------------- Start Dialogue Manager setting for the bot -----------------
 
-/*
-const privateKey = fs.readFileSync(path.join(__dirname, "certs", "privkey.pem"));
-const certificate = fs.readFileSync(path.join(__dirname, "certs", "fullchain.pem"));
-const port = 11443;
-https.createServer({key: privateKey, cert: certificate}, app).listen(port);
-*/
+var bot = new builder.UniversalBot(connector, function(session) {
+    session.send("You said: %s", session.message.text);
+});
 
-// ------------------ End Server setting for the bot ---------------------------
+// ------------------ End Dialogue Manager setting for the bot -----------------
